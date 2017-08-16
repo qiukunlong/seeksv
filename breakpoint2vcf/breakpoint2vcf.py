370a8c280e8b612a44901e100842f081e331bbef
Kunlong Qiu <290832867@qq.com>
2017-08-16 10:54:03 +0800

    Update: breakpoint2vcf.py

--- breakpoint2vcf/breakpoint2vcf.py
+++ breakpoint2vcf/breakpoint2vcf.py
@@ -2,11 +2,13 @@
 import sys
 import argparse
 import vcf
+import re
 
 
 def parser():
 	parser = argparse.ArgumentParser(prog="breakpoint2vcf.py", description="Change seeksv results to vcf")
 	parser.add_argument("breakpoint", help="Input breakpoint file")
+	parser.add_argument("template_vcf", help="Input template vcf file")
 	parser.add_argument("vcf_file", help="Output vcf file")
 	return parser
 
@@ -14,49 +16,76 @@ base_reverse_complementary = {'A' : 'T', 'T' : 'A', 'C' : 'G', 'G' : 'C', 'a' : 
 
 
 def breakpoint2vcfRecord(breakpoint_dict, breakpoint_id):	
+	left_pos = int(breakpoint_dict['left_pos'])
+	right_pos = int(breakpoint_dict['right_pos'])
 	if breakpoint_dict['left_strand'] == '+' and breakpoint_dict['right_strand'] == '+':
 		ref1 = breakpoint_dict['left_seq'][-1]
-		alt1 = vcf.model._Breakend(breakpoint_dict['right_chr'], breakpoint_dict['right_pos'], False, True, ref1, None)
+		alt1 = vcf.model._Breakend(breakpoint_dict['right_chr'], right_pos, False, True, ref1, None)
 		ref2 = breakpoint_dict['right_seq'][0]
-		alt2 = vcf.model._Breakend(breakpoint_dict['left_chr'], breakpoint_dict['left_pos'], True, False, ref2, None)
+		alt2 = vcf.model._Breakend(breakpoint_dict['left_chr'], left_pos, True, False, ref2, None)
 	elif breakpoint_dict['left_strand'] == '+' and breakpoint_dict['right_strand'] == '-':
 		ref1 = breakpoint_dict['left_seq'][-1]
-		alt1 = vcf.model._Breakend(breakpoint_dict['right_chr'], breakpoint_dict['right_pos'], True, True, ref1, None)
+		alt1 = vcf.model._Breakend(breakpoint_dict['right_chr'], right_pos, True, True, ref1, None)
 		ref2 = base_reverse_complementary[breakpoint_dict['right_seq'][0]]
-		alt2 = vcf.model._Breakend(breakpoint_dict['left_chr'], breakpoint_dict['left_pos'], True, False, ref2, None)
-		
+		alt2 = vcf.model._Breakend(breakpoint_dict['left_chr'], left_pos, True, True, ref2, None)
+	elif breakpoint_dict['left_strand'] == '-' and breakpoint_dict['right_strand'] == '+':
+		ref1 = base_reverse_complementary[breakpoint_dict['left_seq'][-1]]
+		alt1 = vcf.model._Breakend(breakpoint_dict['right_chr'], right_pos, False, False, ref1, None)
+		ref2 = breakpoint_dict['right_seq'][0]
+		alt2 = vcf.model._Breakend(breakpoint_dict['left_chr'], left_pos, False, False, ref2, None)
+	
+	breakend_up = "bnt" + str(breakpoint_id) + "_U"
+	breakend_down = "bnt" + str(breakpoint_id) + "_D"
+
 	record1 = vcf.model._Record(\
 			CHROM=breakpoint_dict['left_chr'], \
-			ID="bnt" + str(breakpoint_id) + "_U", \
-			POS=breakpoint_dict['left_pos'],  \
+			ID=breakend_up, \
+			POS=left_pos,  \
 			REF=ref1, \
 			ALT=[alt1], \
 			QUAL='.', \
 			FILTER='PASS', \
-			INFO={}, \
+			INFO={'PARID' : breakend_up, 'MATEID' : breakend_down}, \
 			FORMAT='.', \
 			sample_indexes='.')
 
 	record2 = vcf.model._Record(\
 			CHROM=breakpoint_dict['right_chr'], \
-			ID="bnt" + str(breakpoint_id) + "_D", \
-			POS=breakpoint_dict['right_pos'],  \
+			ID=breakend_down, \
+			POS=right_pos,  \
 			REF=ref2, \
 			ALT=[alt2], \
 			QUAL='.', \
 			FILTER='PASS', \
-			INFO={}, \
+			INFO={'PARID' : breakend_down, 'MATEID' : breakend_up}, \
 			FORMAT='.', \
 			sample_indexes='.')
 	return record1, record2
 
+def readBreakpointFile(breakpoint_file, template_vcf, vcf_file):
+	fin = open(breakpoint_file, 'r')
+	header = next(fin)
+	if re.match(r'@(.+)', header):
+		header = header.replace('@', '')
+		header = header.strip()
+		header_items = header.split("\t")
+	else:
+	 	sys.stderr.write("Error: breapoint file header should be start with '@'\n")
+		exit(1)
+	vcf_writer = vcf.Writer(open(vcf_file, 'w'), vcf.Reader(filename=template_vcf))
+	breakpoint_id = 1
+	for line in fin:
+	 	line = line.strip()
+		items = line.split("\t")
+		breakpoint_dict = dict(zip(header_items, items))
+		record1, record2 = breakpoint2vcfRecord(breakpoint_dict, breakpoint_id)
+		vcf_writer.write_record(record1)
+		vcf_writer.write_record(record2)
+		breakpoint_id += 1
+
 def main():
 	args = parser().parse_args(sys.argv[1:])
-	in_vcf = args.breakpoint
-	out_vcf = args.vcf_file
-	vcf_writer = vcf.Writer(open(out_vcf, 'w'), vcf.Reader(filename=in_vcf))
-	record = vcf.model._Record(CHROM=20, ID='.', POS=1110696, REF='A', ALT=[vcf.model._Substitution('G'), vcf.model._Substitution('T')], QUAL='.', FILTER='.', INFO={}, FORMAT='.', sample_indexes='.')
-	vcf_writer.write_record(record)
+	readBreakpointFile(args.breakpoint, args.template_vcf, args.vcf_file)
 
 	return 0
 
